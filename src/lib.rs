b4d193524b1715f129fb591c3ee7ab2cb3f28619
Gauravjot Garaya <gauravjot@outlook.com>
2023-05-15 00:02:34 -0700

    set worker to loop to look for new jobs

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,7 @@ pub struct Worker {
 
 impl Worker {
   fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-    let thread = thread::spawn(move ||{
+    let thread = thread::spawn(move || loop{
       let job = receiver.lock().unwrap().recv().unwrap();
       println!("Worker {} got a job.", id);
 
